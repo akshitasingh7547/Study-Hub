ea41d8eb5ce658caa66787aab53fa5c1827fc82f
Akshita Singh <akshitasingh7547@gmail.com>
2026-07-08 20:42:26 +0530

    Create studySounds.js

---
+++ src/pages/data/studySounds.js
@@ -0,0 +1,39 @@
+// data/studySounds.js
+
+export const studySounds = [
+  {
+    title: "Rain Sounds",
+    emoji: "🌧",
+    url: "https://mynoise.net/NoiseMachines/rainNoiseGenerator.php"
+  },
+  {
+    title: "Coffee Shop",
+    emoji: "☕",
+    url: "https://coffitivity.com/"
+  },
+  {
+    title: "Lofi Girl",
+    emoji: "🎼",
+    url: "https://www.youtube.com/@LofiGirl"
+  },
+  {
+    title: "Piano",
+    emoji: "🎹",
+    url: "https://www.youtube.com/results?search_query=peaceful+piano+study+music"
+  },
+  {
+    title: "Forest",
+    emoji: "🌲",
+    url: "https://asoftmurmur.com/"
+  },
+  {
+    title: "Ocean Waves",
+    emoji: "🌊",
+    url: "https://asoftmurmur.com/"
+  },
+  {
+    title: "White Noise",
+    emoji: "📚",
+    url: "https://mynoise.net/"
+  }
+]

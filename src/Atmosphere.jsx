9327efcfc690bdeec3aa8157cfe64014827c7baf
Akshita Singh <akshitasingh7547@gmail.com>
2026-07-09 15:02:23 +0530

    Update Atmosphere.jsx

--- src/Atmosphere.jsx
+++ src/Atmosphere.jsx
@@ -1,38 +1,24 @@
 import React from 'react'
+import cloud from './pages/data/public/cloud.png'
 
 export default function Atmosphere() {
   return (
     <>
+      {/* Moon */}
       <div className="magic-moon" />
 
+      {/* Fog */}
       <div className="magic-fog" />
 
+      {/* Candle */}
       <div className="candle-glow" />
 
-      <img
-        src="/cloud.png"
-        className="magic-cloud"
-        alt=""
-      />
+      {/* Clouds */}
+      <img src={cloud} className="magic-cloud cloud1" alt="" />
+      <img src={cloud} className="magic-cloud cloud2" alt="" />
+      <img src={cloud} className="magic-cloud cloud3" alt="" />
 
-      <img
-        src="/cloud.png"
-        className="magic-cloud"
-        alt=""
-      />
-
-      {Array.from({ length: 25 }).map((_, i) => (
-        <div
-          key={`sparkle-${i}`}
-          className="magic-sparkle"
-          style={{
-            left: `${Math.random() * 100}%`,
-            animationDelay: `${Math.random() * 8}s`,
-            animationDuration: `${6 + Math.random() * 6}s`,
-          }}
-        />
-      ))}
-
+      {/* Stars */}
       {Array.from({ length: 80 }).map((_, i) => (
         <div
           key={`star-${i}`}
@@ -41,6 +27,19 @@ export default function Atmosphere() {
             left: `${Math.random() * 100}%`,
             top: `${Math.random() * 100}%`,
             animationDelay: `${Math.random() * 4}s`,
+          }}
+        />
+      ))}
+
+      {/* Sparkles */}
+      {Array.from({ length: 25 }).map((_, i) => (
+        <div
+          key={`sparkle-${i}`}
+          className="magic-sparkle"
+          style={{
+            left: `${Math.random() * 100}%`,
+            animationDelay: `${Math.random() * 8}s`,
+            animationDuration: `${6 + Math.random() * 6}s`,
           }}
         />
       ))}
